3366115b3572be63f4a446ce1f14b3e336bb7125
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-16 18:52:45 +0100

    Added picture

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,11 +2,17 @@
 
 import './Home.scss';
 import {data} from '../common/data';
+import picture from '../res/picture.jpg';
 
 function Home(props) {
   // TODO : Lang
   return (
     <div className="Home">
+
+      <div class="picture-card">
+        <img class="picture" alt="Picture" src={picture} />
+      </div>
+
       <h1>Célian Raimbault</h1>
       <h2>{data.job}</h2>
     </div>
