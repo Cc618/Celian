58137caf6f65570ced7257adf7ebbc40cfd0d5f6
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-17 19:49:53 +0100

    Added hint

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,28 @@ import './Home.scss';
 import {data} from '../common/data';
 import picture from '../res/picture.jpg';
 
+function IconButton(props) {
+  return (
+    <button class="icon-card" onClick={props.onClick}>
+      <i class="material-icons">{props.icon}</i>
+      {/* <div class="hint"> */}
+      {/* TODO : Hint can be null */}
+        <p class="hint">{props.hint}</p>
+      {/* </div> */}
+    </button>
+  );
+}
+
 function Home(props) {
   // TODO : Lang
   return (
     <div className="Home">
       <header>
+          {/* TODO : Clicks + hints */}
           {/* TODO : Github, Linkedin... + Make class */}
-        <button class="icon-card">
-          <i class="material-icons">call</i>
-        </button>
-        {/* <div class="icon-card">
-          <span class="material-icons">mail</span>
-        </div> */}
+          <IconButton icon="call" hint="06 06 06 06 06"
+            onClick={() => console.log("call")} />
+          {/* <IconButton icon="mail" onClick={() => console.log("mail")} /> */}
       </header>
 
       <div class="content">
