450a74d37326227f4b424d6b0006686453336e66
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-27 12:03:45 +0100

    projects: Created class

---
+++ src/pages/Projects.js
@@ -0,0 +1,13 @@
+// Projects + footer
+
+import './Projects.scss';
+import {data} from '../common/data';
+
+function Projects(props) {
+  return (
+    <div className="Projects" id="projects">
+    </div>
+  );
+}
+
+export default Projects;
