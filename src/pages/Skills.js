460cf9b499eb29e2fd5b7c52f81c1428cc61229e
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-30 17:53:42 +0100

    skills: Changed layout to grid

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -7,9 +7,28 @@ import {SkillCard, IconSkillCard} from '../widgets/SkillCard';
 function Skills() {
   return (
     <div className="Skills" id="skills">
-      {data.skills.map((skill, i) => <SkillCard key={i} skill={skill} />)}
-      <IconSkillCard />
+      <div className="wrapper">
+        {/* <div className="tst left top"><div className="tst-1"></div></div>
+        <div className="tst right top"><div className="tst-2"></div></div>
+        <div className="tst left bottom"><div className="tst-2"></div></div>
+        <div className="tst right bottom"><div className="tst-1"></div></div> */}
+        {data.skills.map((skill, i) => <SkillCard key={i} skill={skill} />)}
+        <IconSkillCard />
+      </div>
     </div>
+
+    // <div className="tst">
+    //   <div className="tst-content">
+    //     <div className="red" />
+    //     <div className="blue" />
+    //     <div className="blue" />
+    //     <div className="red" />
+    //     {/* <div className="tst left top"><div className="tst-1"></div></div>
+    //     <div className="tst right top"><div className="tst-2"></div></div>
+    //     <div className="tst left bottom"><div className="tst-2"></div></div>
+    //     <div className="tst right bottom"><div className="tst-1"></div></div> */}
+    //   </div>
+    // </div>
   );
 }
 
