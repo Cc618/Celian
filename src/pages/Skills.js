d95303b045782ff7e2cf58cc300865c9f9dc0049
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-30 12:15:43 +0100

    skills: Added icon skill card (basic)

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -2,14 +2,14 @@
 
 import './Skills.scss';
 import {data} from '../common/data';
-import SkillCard from '../widgets/SkillCard';
+import {SkillCard, IconSkillCard} from '../widgets/SkillCard';
 
 function Skills() {
   return (
     <div className="Skills" id="skills">
       {data.skills.map((skill, i) => <SkillCard key={i} skill={skill} />)}
       {/* TODO : Icon skills card */}
-      <SkillCard skill={{title: "WIP", items: ["wip..."]}} />
+      <IconSkillCard />
     </div>
   );
 }
--- src/widgets/SkillCard.js
+++ src/widgets/SkillCard.js
@@ -2,6 +2,14 @@
 // TODO : Language skill card
 
 import './SkillCard.scss';
+import python from '../res/python.png';
+import cpp from '../res/cpp.png';
+import flutter from '../res/flutter.png';
+import julia from '../res/julia.png';
+import linux from '../res/linux.png';
+import vim from '../res/vim.png';
+import vscode from '../res/vscode.png';
+import git from '../res/git.png';
 
 function SkillCard(props) {
   let skill = props.skill;
@@ -23,23 +31,37 @@ function SkillCard(props) {
 
 // langIcon, hint, ratio, softIcon, skill
 function IconSkillRow(props) {
-
+  return (
+    <tr className="row">
+      <th><img src={props.langIcon} alt="" /></th>
+      {/* TODO : Hint */}
+      {/* TODO : Ratio */}
+      <th><img src={props.softIcon} alt="" /></th>
+      <th><p>{props.skill}</p></th>
+    </tr>
+  );
 }
 
 // Bottom right card
 function IconSkillCard(props) {
-  let skill = props.skill;
-
   return (
     <div className="SkillCard">
       <div className="skill-card">
         <div className="decoration" />
         <div className="content">
-          <h4>{skill.title}</h4>
+          {/* TODO : Translate */}
+          <h4>Compétences</h4>
+          {/* TODO : Translate */}
+          <table>
+            <IconSkillRow langIcon={python} hint="Python" ratio={1} softIcon={linux} skill="OOP" />
+            <IconSkillRow langIcon={cpp} hint="C++" ratio={1} softIcon={vim} skill="Functional Programming" />
+            <IconSkillRow langIcon={flutter} hint="Dart / Flutter" ratio={6 / 10} softIcon={vscode} skill="Data Structures" />
+            <IconSkillRow langIcon={julia} hint="Julia" ratio={5 / 10} softIcon={git} skill="Project Management" />
+          </table>
         </div>
       </div>
     </div>
   );
 }
 
-export default SkillCard;
+export {SkillCard, IconSkillCard};
