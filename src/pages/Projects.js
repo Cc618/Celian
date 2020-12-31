ef451953602c043814cc27e586cdb203e4794e84
Celian RAIMBAULT <celian.raimbault@epita.fr>
2020-12-31 14:06:50 +0100

    projects: Added basic project card widget

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -4,20 +4,41 @@ import './Projects.scss';
 import {data} from '../common/data';
 import Dots from '../widgets/Dots';
 import github from '../res/github_light.png';
+import dart from '../res/flutter.png';
+import quick_shop from '../res/quick_shop.jpg';
+import google_play_badge from '../res/google_play_badge.png';
+
+// title tags icon image description trailing
+function ProjectCard(props) {
+  return (
+    <div className="project-wrapper">
+      <div className="project-card">
+        <div className="header">
+          <h4>{props.title}</h4>
+          {props.tags.map((tag, i) => <p className="tag" key={i}>{tag}</p>)}
+          <img src={props.icon} className="icon" />
+        </div>
+        <img src={props.image} />
+        <p>{props.description}</p>
+        {/* TODO : trailing : props.trailing !== undefined && */}
+      </div>
+    </div>
+  );
+}
 
 function Projects(props) {
+  // TODO : rm
+  let description = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.';
+
   return (
     <div className="Projects" id="projects">
       <div className="bg bg-grey">
 
-        {/* TODO : rm blue / green */}
-        <div className="project-wrapper blue">
-          <div className="project-card">
+        {/* TODO : Translate */}
+        <ProjectCard title="Quick-Shop" tags={["Flutter", "Mobile App"]}
+          icon={dart} image={quick_shop} description={description} />
 
-          </div>
-        </div>
-
-        <div className="project-wrapper green">
+        <div className="project-wrapper">
           <div className="project-card">
 
           </div>
@@ -27,25 +48,25 @@ function Projects(props) {
       <Dots leftColor="accent" rightColor="grey" />
       <div className="bg bg-accent">
 
-        <div className="project-wrapper blue">
+        <div className="project-wrapper">
           <div className="project-card">
 
           </div>
         </div>
 
-        <div className="project-wrapper green">
+        <div className="project-wrapper">
           <div className="project-card">
 
           </div>
         </div>
 
-        <div className="project-wrapper green">
+        <div className="project-wrapper">
           <div className="project-card">
 
           </div>
         </div>
 
-        <div className="project-wrapper blue">
+        <div className="project-wrapper">
           <div className="project-card">
 
           </div>
