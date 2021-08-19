cbe2e96d31432b5ecd9a3b3ac4bb907659a4320d
Celian RAIMBAULT <celian.raimbault@epita.fr>
2021-08-19 15:23:11 +0200

    experiences: Updated card layout, centered title and button

--- src/widgets/ExperienceCard.js
+++ src/widgets/ExperienceCard.js
@@ -8,37 +8,39 @@ function ExperienceCard(props) {
     <div className="ExperienceCard">
       <p className="experience-out experience-year print-rm">{props.experience.year}</p>
       <div className="experience-card">
-        <div className="content">
-          <div className="content-title">
-            <h4>{props.experience.title}</h4>
-            <h3 className="print-experience-year print-add">{props.experience.year}</h3>
-          </div>
-          <p dangerouslySetInnerHTML={{__html: props.experience[`content_${lang}`]}}></p>
-        </div>
-        <div className="experience-button-wrapper print-rm">
-          <div className="experience-button">
-            <div className="button" onClick={() => {
-              App.showPopup(<Popup title={props.experience.title} content={
-                  <div>
-                    <h2>{props.experience.description}</h2>
-                    <h3>{data.personal_contributions}</h3>
-                    <p dangerouslySetInnerHTML={{__html: props.experience.personal_contributions}} />
-                    <h3>{data.professional_contributions}</h3>
-                    <p dangerouslySetInnerHTML={{__html: props.experience.professional_contributions}} />
-                    <h3>{data.technologies}</h3>
-                    <p dangerouslySetInnerHTML={{__html: props.experience.technologies}} />
-                  </div>
-                } />);
-            }}>
-              <i className="material-icons">notes</i>
+        <h4>{props.experience.title}</h4>
+        <div className="experience-card-content">
+          <div className="content">
+            <div className="content-title">
+              <h3 className="print-experience-year print-add">{props.experience.year}</h3>
             </div>
+            <p dangerouslySetInnerHTML={{__html: props.experience[`content_${lang}`]}}></p>
           </div>
+          <div className="experience-button-wrapper print-rm">
+            <div className="experience-button">
+              <div className="button" onClick={() => {
+                App.showPopup(<Popup title={props.experience.title} content={
+                    <div>
+                      <h2>{props.experience.description}</h2>
+                      <h3>{data.personal_contributions}</h3>
+                      <p dangerouslySetInnerHTML={{__html: props.experience.personal_contributions}} />
+                      <h3>{data.professional_contributions}</h3>
+                      <p dangerouslySetInnerHTML={{__html: props.experience.professional_contributions}} />
+                      <h3>{data.technologies}</h3>
+                      <p dangerouslySetInnerHTML={{__html: props.experience.technologies}} />
+                    </div>
+                  } />);
+              }}>
+                <i className="material-icons">notes</i>
+              </div>
+            </div>
 
-          {/* Hint */}
-          <div className="hint">
-            <div className="hint-wrapper">
-              <div className="hint-card">
-                <p>Details</p>
+            {/* Hint */}
+            <div className="hint">
+              <div className="hint-wrapper">
+                <div className="hint-card">
+                  <p>Details</p>
+                </div>
               </div>
             </div>
           </div>
